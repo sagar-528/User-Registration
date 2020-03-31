82b4e513a96c19c568f1686a397374c4dae15589
sagar-528 <gupta.sagar528@gmail.com>
2020-03-31 22:10:04 +0530

    ADDED: Last Name Validation

--- src/main/java/UserValidate.java
+++ src/main/java/UserValidate.java
@@ -3,11 +3,21 @@ import java.util.regex.Pattern;
 
 public class UserValidate
 {
+    // Method for first name.
     public boolean firstName(String fName)
     {
         String patternFirstName="^[A-Z]{1}[a-z]{2,}$";
         Pattern p = Pattern.compile(patternFirstName);
         Matcher matcher = p.matcher(fName);
+        return matcher.matches();
+    }
+
+    //Method for last name.
+    public boolean lastName(String lName)
+    {
+        String patternLastName="^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(patternLastName);
+        Matcher matcher = p.matcher(lName);
         return matcher.matches();
     }
 }
--- src/test/java/Data_Test.java
+++ src/test/java/Data_Test.java
@@ -7,10 +7,19 @@ public class Data_Test
 {
     UserValidate validate = new UserValidate();
 
+    // Test case for validating first name.
     @Test
     public void ProvideFirstName_WhenProper_ReturnTrue()
     {
         boolean result = validate.firstName("Pawan");
+        Assert.assertEquals(true, result );
+    }
+
+    // Test case for validating last name.
+    @Test
+    public void ProvideLastName_WhenProper_ReturnTrue()
+    {
+        boolean result = validate.lastName("Gupta");
         Assert.assertEquals(true, result );
     }
 }
